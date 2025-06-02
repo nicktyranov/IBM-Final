5d1ead4a89e9376ff2957f7f3b8a719226d03c17
Nick Tyranov <nicktyranov@gmail.com>
2025-06-01 22:23:33 -0600

    handleRegister

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import './RegisterPage.css';
 
 function RegisterPage() {
@@ -7,9 +9,39 @@ function RegisterPage() {
 	const [lastName, setLastName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
 
-	const handleRegister = () => {
-		console.log('clicked Register');
+	const navigate = useNavigate();
+	const { setIsLoggedIn } = useAppContext();
+
+	const handleRegister = async () => {
+		try {
+			const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json'
+				},
+				body: JSON.stringify({
+					firstName: firstName,
+					lastName: lastName,
+					email: email,
+					password: password
+				})
+			});
+			const json = await response.json();
+			if (json.authtoken) {
+				sessionStorage.setItem('auth-token', json.authtoken);
+				sessionStorage.setItem('name', firstName);
+				sessionStorage.setItem('email', json.email);
+				setIsLoggedIn(true);
+				navigate('/app');
+			}
+			if (json.error) {
+				setError(json.error);
+			}
+		} catch (e) {
+			console.log('Error fetching details: ' + e.message);
+		}
 	};
 
 	return (
@@ -17,7 +49,8 @@ function RegisterPage() {
 			<div className="row justify-content-center">
 				<div className="col-md-6 col-lg-4">
 					<div className="register-card p-4 border rounded">
-						<h2 className="text-center mb-4 font-weight-bold">Register</h2>
+						<h2 className="text-center mb-4 font-weight-bold">Register</h2>1
+						<div className="text-danger">{error}</div>
 						<label htmlFor="firstName" className="form label">
 							First Name:
 						</label>
@@ -27,7 +60,6 @@ function RegisterPage() {
 							onChange={(e) => setFirstName(e.target.value)}
 							placeholder="Enter your first name"
 						/>
-
 						<label htmlFor="lastName" className="form label">
 							Last Name:
 						</label>
@@ -37,7 +69,6 @@ function RegisterPage() {
 							onChange={(e) => setLastName(e.target.value)}
 							placeholder="Enter your last name"
 						/>
-
 						<label htmlFor="email" className="form label">
 							Email:
 						</label>
@@ -47,7 +78,6 @@ function RegisterPage() {
 							onChange={(e) => setEmail(e.target.value)}
 							placeholder="Enter your email"
 						/>
-
 						<label htmlFor="password" className="form label">
 							Password:
 						</label>
